e13ba70e2fe54075c10d13ee615d0b96ea1be467
ihip <ivan.hip@gmail.com>
2021-07-11 12:54:48 +0200

    metropolis: original Graz version

---
+++ u1/v_dummy.f
@@ -0,0 +1,15 @@
+      character*32 function v_invert()
+      v_invert = 'none'
+      end
+
+      character*32 function v_invert_b()
+      v_invert_b = 'none'
+      end
+
+      character*32 function v_invert1()
+      v_invert1 = 'none'
+      end
+
+      character*32 function v_invert1_b()
+      v_invert1_b = 'none'
+      end
